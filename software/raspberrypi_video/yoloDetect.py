d9de206b07827a68882db713b94355d43f2b8d98
tbest2 <158211026+tbest2@users.noreply.github.com>
2025-08-06 14:09:00 -0700

    updated yolov8n model, leaving old one here just in case

--- software/raspberrypi_video/yoloDetect.py
+++ software/raspberrypi_video/yoloDetect.py
@@ -3,7 +3,8 @@ from ultralytics import YOLO
 
 
 #loads yolo model
-model = YOLO("/home/ubuntu/Downloads/thermal/LeptonModule/software/raspberrypi_video/best.pt")
+#model = YOLO("/home/ubuntu/Downloads/thermal/LeptonModule/software/raspberrypi_video/best.pt")
+model = YOLO("/home/ubuntu/Downloads/thermal/LeptonModule/software/raspberrypi_video/bestV6.pt")
 
 #this runs and saves the YOLO model results to this variable
 #you can also run the model on an entire folder of images much faster per image than a singular image
@@ -16,5 +17,4 @@ results[0].show()
 #this prints the bounding boxes to terminal
 #print(f"bounding boxes: {results[0].boxes}")
 print(f"bounding boxes: {results[0].boxes.xyxy}")
-
 
